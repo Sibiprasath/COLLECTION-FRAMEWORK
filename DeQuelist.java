19136578bd24bd686aee90b277aef0624dab2024
Sibiprasath S <sibi45prasath@gmail.com>
2024-08-14 16:16:27 +0530

    new file

---
+++ DeQuelist.java
@@ -0,0 +1,44 @@
+
+import java.util.ArrayDeque;
+
+
+public class DeQuelist{
+    public static void main(String[] args) {
+        ArrayDeque Q=new ArrayDeque();
+        Q.add(10);
+        Q.add(20);
+        Q.add(30);
+        Q.add(40);
+        Q.add(50);
+        System.out.println(Q);
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~");
+
+        Q.addFirst(5);
+        Q.addLast(55);
+        System.out.println(Q);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~");
+
+        //remove
+        //poll
+        //pop
+
+        /* Q.remove();
+        System.out.println(Q);
+
+        Q.pop();
+        System.out.println(Q);
+
+        Q.poll();
+        System.out.println(Q);*/
+
+        Q.remove(30);
+        System.out.println(Q);
+
+        Q.add("Nandha");
+        Q.add('S');
+        Q.add(3.1234f);
+        System.out.println(Q);
+
+    }
+}
---
+++ PriorityQue.java
@@ -0,0 +1,20 @@
+import java.util.PriorityQueue;
+
+public class PriorityQue{
+    public static void main(String[] args) {
+        PriorityQueue<Integer> que= new PriorityQueue<>();
+        
+        que.add(50);
+        que.add(80);
+        que.add(60);
+        que.add(90);
+        que.add(40);
+        que.add(20);
+
+        System.out.println(que);
+        // remove
+        // clear
+        // contains
+
+    }
+}
